e0868eca9d42361345aac349524dc0bb4a2e681c
Brad Sickles <brad.sickles@gmail.com>
2022-05-09 15:31:16 -0400

    Set up docker-compose

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,11 @@ import Config
 
 # Configure your database
 config :pubsublive, Pubsublive.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "pubsublive_dev",
+  url: System.get_env("DATABASE_URL"),
+#  username: "postgres",
+#  password: "postgres",
+#  hostname: "localhost",
+#  database: "pubsublive_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,7 +20,7 @@ config :pubsublive, Pubsublive.Repo,
 config :pubsublive, PubsubliveWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [ip: {0, 0, 0, 0}, port: 4000],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
