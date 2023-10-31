fe8fd37a90a7ad3bad17c59ce7d8288e2eb3734c
Brad Sickles <brad.sickles@gmail.com>
2023-10-31 14:17:36 -0400

    Fix docker setup

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,14 +53,9 @@ if config_env() == :prod do
 
   config :pubsublive, PubsubliveWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
+    # Binding to loopback ipv4 address prevents access from other machines.
+    # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
+    http: [ip: {0, 0, 0, 0}, port: port],
     secret_key_base: secret_key_base
 
   # ## Configuring the mailer
